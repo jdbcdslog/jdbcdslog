33275b499e55487ea1c74dd86db10a041b058db3
lishunli.me@gmail.com <lishunli.me@gmail.com@f57c63fb-bdfd-6822-b309-70371a8284da>
2011-09-09 03:54:01 +0000

    DEV: 1. rename package com.googlecode.usc.jdbcdslog to org.jdbcdslog for no need change config. 2. correct slf4j group id.
    git-svn-id: https://jdbcdslog-exp.googlecode.com/svn/trunk@1370 f57c63fb-bdfd-6822-b309-70371a8284da

---
+++ jdbcdslog/src/main/java/org/jdbcdslog/LogUtils.java
@@ -0,0 +1,105 @@
+package org.jdbcdslog;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@SuppressWarnings("rawtypes")
+public class LogUtils {
+
+    static Logger logger = LoggerFactory.getLogger(LogUtils.class);
+
+    public static void handleException(Throwable e, Logger l, StringBuffer msg) throws Throwable {
+        if (e instanceof InvocationTargetException) {
+            Throwable t = ((InvocationTargetException) e).getTargetException();
+            if (l.isErrorEnabled())
+                l.error(msg + "\nthrows exception: " + t.getClass().getName() + ": " + t.getMessage(), t);
+            throw t;
+        } else {
+            if (l.isErrorEnabled())
+                l.error(msg + "\nthrows exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public static StringBuffer createLogEntry(Method method, Object sql, String parameters, String namedParameters) {
+        String methodName = "createLogEntry() ";
+        if (logger.isDebugEnabled())
+            logger.debug(methodName);
+        StringBuffer s = new StringBuffer(method.getDeclaringClass().getName()).append(".").append(method.getName());
+        s.append(" ");
+        if (sql != null)
+            s.append(sql);
+        if (parameters != null) {
+            s.append(" parameters: ");
+            s.append(parameters);
+        }
+        if (namedParameters != null) {
+            s.append(" named parameters: ");
+            s.append(namedParameters);
+        }
+        return s;
+    }
+
+    public static StringBuffer createLogEntry(String sql, TreeMap parameters) {
+        StringBuffer s = new StringBuffer();
+
+        if (sql != null) {
+            int questionMarkCount = 1;
+            Pattern p = Pattern.compile("\\?");
+            Matcher m = p.matcher(sql);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while (m.find()) {
+                m.appendReplacement(stringBuffer, sqlValueToString(parameters.get(questionMarkCount)));
+                questionMarkCount++;
+            }
+            sql = String.valueOf(m.appendTail(stringBuffer));
+
+            s.append(sql).append(";");
+        }
+
+        return s;
+    }
+
+    public static String sqlValueToString(Object object) {
+        /*
+         * if (o == null) return "null"; if (o instanceof String) return "'" + o.toString() + "'"; else if (o instanceof Reader && ConfigurationParameters.logText) { return readFromReader((Reader) o); } else return o.toString();
+         */
+        if (object == null) {
+            return "null";
+        } else if (object instanceof String) {
+            //handle replaceAll's special characters: \ \$
+            String value = ((String) object).replaceAll("\\\\", "\\\\\\\\").replaceAll("\\$", "\\\\\\$");
+            // handle Oracle sql's special characters,like ' &
+            value = value.replaceAll("'", "''")
+                         .replaceAll("&","'||chr(38)||'");
+
+            return "'" + value + "'";
+        } else if (object instanceof Timestamp) {
+            return "to_timestamp('" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(object) + "', 'yyyy-MM-dd hh24:mi:ss.ff3')";
+        } else if (object instanceof Date) {
+            return "to_date('" + new SimpleDateFormat("yyyy-MM-dd").format(object) + "', 'yyyy-MM-dd')";
+        } else if (object instanceof Boolean) {
+            return ((Boolean) object).booleanValue() ? "Y" : "N";
+        } else {
+            return object.toString();
+        }
+    }
+
+    public static String getStackTrace() {
+        if (!ConfigurationParameters.printStackTrace)
+            return "";
+        StackTraceElement stackTraces[] = new Throwable().getStackTrace();
+        StringBuffer sb = new StringBuffer(" at ");
+        sb.append(stackTraces[4]);
+        return sb.toString();
+    }
+}
---
+++ jdbcdslog/src/test/java/org/jdbcdslog/LogUtilsTest.java
@@ -0,0 +1,33 @@
+package org.jdbcdslog;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TreeMap;
+
+import org.jdbcdslog.LogUtils;
+import org.junit.Test;
+
+public class LogUtilsTest {
+
+    @Test
+    public void testCreateLogEntry() {
+        String sql = "select * from mc_instr where instr_cde = ? and instcl_id = ? and expr_date = ? and last_upd_time = ?";
+        TreeMap<Integer, Object> parameters = new TreeMap<Integer, Object>();
+        parameters.put(1, "IN\\ST\\$S4R0'3&0'11");
+        parameters.put(2, 1);
+
+        Calendar cal = new GregorianCalendar(2011, 0, 1, 23, 59, 59);
+
+        parameters.put(3, cal.getTime());
+        parameters.put(4, new Timestamp(cal.getTimeInMillis()));
+
+        assertEquals(
+                "select * from mc_instr where instr_cde = 'IN\\ST\\$S4R0''3'||chr(38)||'0''11' and instcl_id = 1 and expr_date = to_date('2011-01-01', 'yyyy-MM-dd') and last_upd_time = to_timestamp('2011-01-01 23:59:59.000', 'yyyy-MM-dd hh24:mi:ss.ff3');",
+                LogUtils.createLogEntry(sql, parameters).toString());
+
+    }
+
+}
